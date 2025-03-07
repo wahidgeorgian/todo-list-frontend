229ed3a32cf3ebb943f9571ff082f0236d21cb53
wahidgeorgian <w3917k@gmail.com>
2025-03-07 12:46:01 +0530

    refactor layout to use Outlet for routing; update Navbar links for proper navigation; adjust AboutPage margin and clean up CSS

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,13 @@
+import { Outlet } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1"><Outlet /></main>
       <Footer />
     </div>
   )
